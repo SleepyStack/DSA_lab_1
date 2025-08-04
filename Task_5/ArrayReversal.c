ff6968014eabc700f88253e55350ebc4767c585e
Chirag <chirag2006chopra@gmail.com>
2025-08-04 12:04:55 +0530

    Update ArrayReversal.c

--- Task_5/ArrayReversal.c
+++ Task_5/ArrayReversal.c
@@ -1,24 +1,31 @@
 #include <stdio.h>
 
-void main() {
+int main() {
     int elementCount;
     printf("How many elements will be in the array: ");
-    scanf("%d",&elementCount);
+    scanf("%d", &elementCount);
+
     int myArray[elementCount];
-    for(int i = 0; i< elementCount; i++){
-        printf("Enter Element %d: ",i);
+    for (int i = 0; i < elementCount; i++) {
+        printf("Enter Element %d: ", i);
         scanf("%d", &myArray[i]);
     }
+
     int left = 0;
     int right = elementCount - 1;
-    for(int i = 0; i <= elementCount/2; i++){
-        int temp = myArray[right];
-        myArray[right] = myArray[left];
-        myArray[left] = temp;
+    while (left < right) {
+        int temp = myArray[left];
+        myArray[left] = myArray[right];
+        myArray[right] = temp;
         left++;
         right--;
     }
-    for(int i = 0; i< elementCount; i++){
-        printf("%d: ",myArray[i]);
+
+    printf("Reversed array: ");
+    for (int i = 0; i < elementCount; i++) {
+        printf("%d ", myArray[i]);
     }
+    printf("\n");
+
+    return 0;
 }
