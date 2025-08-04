eaaf0fe06fe7a078646eafcbbebeacf518a2937c
Chirag <chirag2006chopra@gmail.com>
2025-08-04 12:03:01 +0530

    Create ElementFrequency.c
    A program to calculate the times an element occurs in given array.

---
+++ Task_6/ElementFrequency.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+void main() {
+    int elementCount;
+    printf("How many elements will be in the array: ");
+    scanf("%d",&elementCount);
+    int myArray[elementCount];
+    for(int i = 0; i< elementCount; i++){
+        printf("Enter Element %d: ",i);
+        scanf("%d", &myArray[i]);
+    }
+    int checkElement;
+    printf("Enter the number you want to check the frequency of: ");
+    scanf("%d",&checkElement);
+    int elementCounter = 0;
+    for(int i = 0; i< elementCount; i++){
+        if(checkElement == myArray[i]){
+            elementCounter += 1;
+        }
+    }
+    printf("Occurance of %d in given array is: %d",checkElement,elementCounter);
+}
