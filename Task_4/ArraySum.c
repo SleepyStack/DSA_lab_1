a8619bf877c5da2905dc86a891fb9d46e039a47f
Chirag <chirag2006chopra@gmail.com>
2025-08-04 12:05:36 +0530

    Update and rename ArraySum to ArraySum.c

--- Task_4/ArraySum.c
+++ Task_4/ArraySum.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 
-void main() {
+int main() {
     int elementCount;
     printf("How many elements will be in the array: ");
     scanf("%d",&elementCount);
@@ -14,4 +14,5 @@ void main() {
         arraySum += myArray[i];
     }
     printf("The Sum is: %d",arraySum);
+    return 0;
 }
